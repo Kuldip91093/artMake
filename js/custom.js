9c0f164581ef8c71e4211b7264e94686e25dcff9
SanjayBarad <baradsanjay2018@gmail.com>
2022-04-19 00:54:46 +0530

    Fixed Design Changes @SanjayBarad

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,9 @@
 // on page scroll animations js
-$(window).on('load',function() {
+$(window).on('load', function () {
 	$('.loader-wrapper').fadeOut('slow');
-	$(function() {
-		var observer = new IntersectionObserver(function(entries) {
-			entries.forEach(function(e) {
+	$(function () {
+		var observer = new IntersectionObserver(function (entries) {
+			entries.forEach(function (e) {
 				if (!e.isIntersecting) return;
 				e.target.classList.add('move'); // 交差した時の処理
 				observer.unobserve(e.target);
@@ -16,13 +16,13 @@ $(window).on('load',function() {
 				//   e.rootBounds			ルートのgetBoundingClientRect()
 				//   e.time					変更が起こったタイムスタンプ
 			})
-		},{
+		}, {
 			// オプション設定
 			rootMargin: '0px 0px -5% 0px' //下端から5%入ったところで発火
 			//threshold: [0, 0.5, 1.0]
 		});
 		var target = document.querySelectorAll('.io'); //監視したい要素をNodeListで取得
-		for(var i = 0; i < target.length; i++ ) {
+		for (var i = 0; i < target.length; i++) {
 			observer.observe(target[i]); // 要素の監視を開始
 		}
 		//アニメーションによる各要素のはみ出しを解消
@@ -33,19 +33,19 @@ $(window).on('load',function() {
 	});
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
 
 	// navbar toggle js
-	$('.navbar_toggler').click(function(){
+	$('.navbar_toggler').click(function () {
 		$('body').toggleClass('no_scroll');
 		$(this).toggleClass('open_menu');
 		$(this).next("nav").toggleClass('navbar_animate');
 	});
 
 	// got to page top js
-	$(window).on('load scroll',function(){
-		var	windowTop = $(window).scrollTop();
-		if(windowTop > 600) {
+	$(window).on('load scroll', function () {
+		var windowTop = $(window).scrollTop();
+		if (windowTop > 600) {
 			$('.pagetop').fadeIn();
 		} else {
 			$('.pagetop').fadeOut();
@@ -59,18 +59,18 @@ $(document).ready(function(){
 	});
 
 	// FAQ accordian
-	$( function() {
+	$(function () {
 		$(".accordion-jquery-ui").accordion();
 	});
 
 	// before after
 	var window_size = $(window).width();
 
-	if(window_size < 676){
+	if (window_size < 676) {
 		$('.stepper_row').slick({
 			infinite: false,
 			slidesToShow: 1,
-  			slidesToScroll: 1,
+			slidesToScroll: 1,
 			fade: true,
 			arrows: true,
 			dots: true
@@ -78,18 +78,18 @@ $(document).ready(function(){
 		$('.before_after_slider').slick({
 			infinite: false,
 			slidesToShow: 1,
-  			slidesToScroll: 1
+			slidesToScroll: 1
 		});
 	}
 
 	// problem contnet slider JS
 	$('.problem_contnet_slider').slick({
-			dots: false,
-			arrows: false,
-			fade: true,
-			speed: 500,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 4000,
-		});
+		dots: false,
+		arrows: false,
+		fade: true,
+		speed: 500,
+		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 4000,
+	});
 });
